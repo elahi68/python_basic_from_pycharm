1ae953d61e183d29632954fcc1c1b6bb41acf122
elahi68 <shaikelahi68@gmail.com>
2022-03-01 19:15:44 +0530

    server working file with mobile phone after written print in accept

--- client-server/client.py
+++ client-server/client.py
@@ -2,7 +2,8 @@ import socket
 
 c = socket.socket()
 
-c.connect(('localhost',9999))
+c.connect(('192.168.120.151',9999))
+#c.connect(('localhost',9999))
 name = input('Enter your name: ')
 c.send(bytes(name,'utf-8'))
 print(c.recv(1024).decode())
